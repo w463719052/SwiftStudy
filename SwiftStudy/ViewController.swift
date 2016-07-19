ffacc9f4a867523a84d45ce2766f18c17bcc4206
w463719052 <zccl2@zccldemac-mini.local>
2016-07-19 10:10:08 +0800

    Initial Commit

---
+++ SwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by zccl2 on 16/7/19.
+//  Copyright © 2016年 com.zccl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
